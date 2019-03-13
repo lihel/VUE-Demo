3f7fce94fa52cfd83c65150192da189d4739afc3
Ubuntu <835943102@qq.com>
2019-03-13 21:34:51 +0800

    v-if

--- src/Components/component.vue
+++ src/Components/component.vue
@@ -3,7 +3,14 @@
     <h1>test : {{test}}</h1>
     <h1>url : {{url}}</h1>
     <h1>method:{{method()}}</h1>
+    <div id="appp">
+        <span v-if="seen">
+            隐身
+        </span>
+    </div>
   </div>
+
+
 </template>
 
 <script type="text/javascript">
@@ -14,8 +21,11 @@
         url: "www.hahaha.com",
         method: function a() {
           return this.test + "哈哈哈哈"
-        }
+        },
+        seen: false
       }
     }
   }
+
 </script>
+
